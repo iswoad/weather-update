a79c8a6c5fd7dc916a67393de1590096a47efbe3
iswoad <iswoadansary@gmail.com>
2021-02-10 01:08:37 +0600

    fetched data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const getCityWeather = () =>{
     const cityName = document.getElementById('city-search').value;
     console.log(cityName)
-    fetch('https://api.openweathermap.org/data')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=313674b36b8fa24f194dcf261c032c57`)
     .then(res => res.json())
     .then(data => console.log(data))
 }
