b447a646a2ea3ed2b0006676c841f5d0a279939e
iswoad <iswoadansary@gmail.com>
2021-02-10 01:49:45 +0600

    shown city name and temp

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const getCityWeather = () =>{
-    const cityName = document.getElementById('city-search').value;
+    let cityName = document.getElementById('city-search').value;
     console.log(cityName)
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=313674b36b8fa24f194dcf261c032c57`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        showResult(data)
+    })
+}
+const showResult =(update) =>{
+    const showCityName = update.name;
+    const showTemp = update.main.temp;
+    document.getElementById('city-name').innerText =showCityName;
+    document.getElementById('temp-display').innerText =showTemp;
 }
